09264b8e6858d4c9f7eb96051a2aba9cb46e450d
Rick Ree <github@rree.fastmail.fm>
2012-11-07 07:55:10 -0600

    testing bulbs

--- modules/bulbs_util.py
+++ modules/bulbs_util.py
@@ -13,54 +13,29 @@ except Exception as e:
     print 'neo4j server not found:', e
 
 def get_ncbi_node(leaf):
-    label = rec.label
+    label = leaf.rec.label
     if leaf.rec.ottol_name:
         if leaf.rec.ottol_name.ncbi_taxid:
             taxid = str(leaf.rec.ottol_name.ncbi_taxid)
-            return ncbi_node_idx.get('taxid', taxid)[0]
+            return ncbi_node_idx.lookup(taxid=taxid).next()
         else:
             label = leaf.rec.ottol_name.name
     name = label.replace('_', ' ')
-    v = ncbi_name_idx.get('name', name)
+    v = list(ncbi_name_idx.lookup(name=name))
     if len(v)==1:
-        return v[0].get_single_related_node(OUTGOING, 'NAME_OF')
+        return v[0].outV('NAME_OF').next()
 
+rec = db.stree(3)
+root = build.stree(db, rec.id)
+for lf in root.leaves(): lf.ncbi_node = get_ncbi_node(lf)
 
-def insert_stree(db, rec):
-    root = build.stree(db, rec.id)
-    for n in root.leaves():
-        node = None
-        if n.rec.ottol_name:
-            ottol_name = db.ottol_name(n.rec.ottol_name)
-            if ottol_name.ncbi_taxid:
-                taxid = ottol_name.ncbi_taxid
-                node = ncbi_node_idx.get_unique(taxid=taxid)
-            else:
-                name = n.rec.ottol_name.name
-                v = list(ncbi_name_idx.lookup(name=name))
-                if len(v)==1:
-                    node = v[0].outV('NAME_OF').next()
-                elif len(v)>1:
-                    print len(v), 'synonyms of ottol_name', name
-                else: print 'ottol_name', name, 'not found'
-        else:
-            name = n.rec.label
-            v = list(ncbi_name_idx.lookup(name=name))
-            if len(v)==1:
-                node = v[0].outV('NAME_OF').next()
-            elif len(v)>1:
-                print len(v), 'synonyms of label', name
-            else: print 'label', name, 'not found'
-        n.ncbi_node = node
-    nodes = [ n.ncbi_node for n in root.leaves() ]
-
-    q = """
-    leaves = g.v%(leaves)s
-    v = []
-    leaves.each() {
-        leaf -> v.add(leaf.out('CHILD_OF').loop(1){true}{true}*.id)
-    }
-    v
-    """ % dict(leaves=tuple([ x.eid for x in nodes ]))
-    resp = G.gremlin.execute(q).content
+q = """
+v = []
+leafids.each() {
+    leaf -> v.add(g.v(leaf).out('CHILD_OF').loop(1){true}{true}*.id)
+}
+v
+"""
+leafids = [ x.ncbi_node.eid for x in root.leaves() if x.ncbi_node ]
+resp = G.gremlin.execute(q, dict(leafids=leafids)).content
 
