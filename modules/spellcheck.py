0c0dd5d3137b4b1f70245beeaac9b119a382e0b0
Rick Ree <github@rree.fastmail.fm>
2013-03-11 19:21:37 -0500

    spellchecker module

---
+++ modules/spellcheck.py
@@ -0,0 +1,42 @@
+from gluon.html import *
+import re, enchant
+DIGITS = re.compile(r'\d')
+INFRANK = re.compile(r'(\s+(ssp|subsp|var|forma|f)([.]?)(?=\s+))', re.I)
+NAMES = 'applications/phylografter/static/names.txt'
+d = enchant.request_pwl_dict(NAMES)
+
+def check(s):
+    return d.check(s)
+
+def suggest(s):
+    return d.suggest(s)
+
+def process_label(db, otu):
+    options = []
+    words = otu.label.replace('.',' ').replace('_',' ').split()
+    if words[-1].lower() == 'sp':
+        words = words[:-1]
+    s = DIGITS.sub('', ' '.join(words))
+    if check(s):
+        options = list(db(db.ottol_name.name==s).select())
+        return (True, options)
+
+    v = suggest(s)
+    if not v and INFRANK.search(s):
+        s = INFRANK.sub('', s)
+        v = suggest(s)
+    if not v:
+        words = s.split()
+        N = len(words)
+        if N > 2:
+            i = N-1
+            while i >= 2:
+                t = ' '.join(words[:i])
+                v = suggest(t)
+                if v: break
+                else: i -= 1
+    for w in v:
+        for row in db(db.ottol_name.name==w).select():
+            options.append(row)
+    return (False, options)
+
