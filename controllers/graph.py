313eec6b6e2d144cdd8ed8bbae81d15afd5e96d1
Lyndon Coghill <lcoghill@fieldmuseum.org>
2013-10-30 16:54:59 -0500

    Added conditional check to confirm that the desired JSON is present. Cleaned up the code base in general as well.

--- controllers/graph.py
+++ controllers/graph.py
@@ -21,19 +21,21 @@ def view():
         u = URL(c="study",f="view",args=[v])
         return A(_study_rep(db.study(v)), _href=u)
     db.stree.study.widget = w
-    response.subtitle = "Delete source tree %s" % rec.id
+    response.subtitle = "Graph of taxonomy and source tree %s" % rec.id
     fields = ["study", "contributor", "newick", "type",
               "clade_labels_represent", "clade_labels_comment",
               "branch_lengths_represent", "branch_lengths_comment",
               "comment"]
     path =  os.path.abspath("/static/json/" + "/../../")     
     dirList = os.listdir(path) # get a list of all files in the json directory
-    filenamedate = dirList[0][:10]
-    tree
-    if ##FILE EXISTS:
-      json = open('Path/to/file', 'r').read() # read in the entire json as a string
+    file_name_date = dirList[0][:10]
+    file_path = ##
+    json_file_name =  file_path + file_name_date + "tree_" + rec 
+    
+    if any(json_file_name in s for s in dirList): ## Check if target JSON file is present:
+      json = open(json_file_name, 'r').read() # read in the entire json as a string
 
     else:
-      json = "No Json Found."
+      json = "No graph found for tree " + rec
 
   return dict(rec=rec, json=json)
