4e2b388e89e5d4d2edf6c62b3322c774ea8712ae
Lyndon Coghill <lcoghill@fieldmuseum.org>
2013-11-21 18:45:03 -0600

    Modified the read directory so that the controller will correctly locate the JSON files under static/taxonomy-stree-json/

--- controllers/study.py
+++ controllers/study.py
@@ -429,13 +429,12 @@ def view():
     working_dir = os.path.dirname(os.path.realpath(__file__))
     working_dir = working_dir[:-11]
     working_dir = str(working_dir)
-    working_dir = working_dir + "static/treegraphjson/ncbi/"
+    working_dir = working_dir + "static/taxonomy-stree-json/ncbi/"
     dirlist = os.listdir(working_dir)# get list of files in working_directory
-    file_date = dirlist[0][:10]# snip the date stamp from one of those files to know what date to look for
     graphlist = []
     for t in trees:
         treeid = str(t.id)
-        file_name = working_dir + file_date + "_tree_" + treeid + ".JSON" #build filename variable 
+        file_name = working_dir + "tree_" + treeid + ".JSON" #build filename variable 
         try:
             with open(file_name):
                 graphlist.append(treeid)
