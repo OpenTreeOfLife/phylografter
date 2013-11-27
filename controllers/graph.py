ab60f5bb23fc3c930b5f2cdcd347684141b391bf
Lyndon Coghill <lcoghill@fieldmuseum.org>
2013-11-27 11:50:19 -0600

    Added a function to return the json upon request from the view via AJAX after the rest of the document loads. This will allow the rest of the page to load in cases of large graph visualizations that may take a few seconds.

--- controllers/graph.py
+++ controllers/graph.py
@@ -15,8 +15,9 @@ import os
 
 ## View a graph of the chosen tree and taxonomy
 
-def view():
     
+def index():
+
     rec = db.stree(request.args(0))
     treeid = str(rec.id)
     def w(f,v):
@@ -24,14 +25,58 @@ def view():
         return A(_study_rep(db.study(v)), _href=u)
     db.stree.study.widget = w
     response.subtitle = "TreeGraph of source tree %s" % rec.id
+
+
+    return dict(rec=rec)
+
+def view_ncbi():
+
+    rec = db.stree(request.args(0))
+    treeid = str(rec.id)
+    def w(f,v):
+        u = URL(c="study",f="view",args=[v])
+        return A(_study_rep(db.study(v)), _href=u)
+    db.stree.study.widget = w
+    response.subtitle = "TreeGraph of source tree %s" % rec.id
+
+
+    return dict(rec=rec)    
+
+def view_ott():
+
+    rec = db.stree(request.args(0))
+    treeid = str(rec.id)
+    def w(f,v):
+        u = URL(c="study",f="view",args=[v])
+        return A(_study_rep(db.study(v)), _href=u)
+    db.stree.study.widget = w
+    response.subtitle = "TreeGraph of source tree %s" % rec.id
+
+
+    return dict(rec=rec)                   
+
+def graph_ncbi():
+    
+
     working_dir = os.path.dirname(os.path.realpath(__file__))
     working_dir = working_dir[:-11]
     working_dir = str(working_dir)
-    working_dir = working_dir + "static/treegraphjson/ncbi/"
-    dirlist = os.listdir(working_dir)# get list of files in working_directory
-    file_date = dirlist[0][:10]# snip the date stamp from one of those files to know what date to look for
-    json_file_name = working_dir + file_date + "_tree_" + treeid + ".JSON" #build filename variable 
+    working_dir = working_dir + "static/taxonomy-stree-json/ott/"
+    json_file_name = working_dir + "tree_2.JSON" #build filename variable 
     json = open(json_file_name, 'r').read() # read in the entire json as a string
 
 
-    return dict(rec=rec, json=json)
+    return json
+
+def graph_ott():
+    
+
+    working_dir = os.path.dirname(os.path.realpath(__file__))
+    working_dir = working_dir[:-11]
+    working_dir = str(working_dir)
+    working_dir = working_dir + "static/taxonomy-stree-json/ott/"
+    json_file_name = working_dir + "tree_2.JSON" #build filename variable 
+    json = open(json_file_name, 'r').read() # read in the entire json as a string
+
+
+    return json
